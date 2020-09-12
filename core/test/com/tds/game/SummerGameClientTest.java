629b5a5206c0c8aed1f076495dfc4f2817b13950
carl <carl.hume@gmail.com>
2020-09-12 18:14:21 -0400

    Setting up unit testing for the Core project.

---
+++ core/test/com/tds/game/SummerGameClientTest.java
@@ -0,0 +1,14 @@
+package com.tds.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SummerGameClientTest {
+
+    @Test
+    public void testJUnitIsConfigured() {
+        assertTrue( true );
+    }
+
+}
