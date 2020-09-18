6028ff65ec89a20677510a684ab9e610522080a4
carl <carl.hume@gmail.com>
2020-09-18 19:23:00 -0400

    Updated to use Lwjgl3

--- desktop/src/com/tds/game/desktop/DesktopLauncher.java
+++ desktop/src/com/tds/game/desktop/DesktopLauncher.java
@@ -1,15 +1,14 @@
 package com.tds.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.tds.game.SummerGameClient;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Summer Game Client";
-		config.width = 1024;
-		config.height = 768;
-		new LwjglApplication(new SummerGameClient(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle( "Summer Game Client" );
+		config.setWindowedMode(1024, 768 );
+		new Lwjgl3Application(new SummerGameClient(), config);
 	}
 }
