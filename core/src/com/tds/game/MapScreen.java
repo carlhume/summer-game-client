284ae0c689a962669d4f54bf1db7e4c07ce6013e
carl <carl.hume@gmail.com>
2020-09-19 22:14:56 -0400

    Removed unused imports

--- core/src/com/tds/game/MapScreen.java
+++ core/src/com/tds/game/MapScreen.java
@@ -5,9 +5,6 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Scaling;
 
 import java.util.HashMap;
 import java.util.logging.Logger;
